e03d82fc278a1d39ba020bea27939169dea8cf28
Carmen Renee <carmengenpt@gmail.com>
2019-03-18 21:05:37 +0000

    first commit

---
+++ lib/vegan/ideas.rb
@@ -0,0 +1,8 @@
+require "vegan/ideas/version"
+
+module Vegan
+  module Ideas
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/vegan/ideas/version.rb
@@ -0,0 +1,5 @@
+module Vegan
+  module Ideas
+    VERSION = "0.1.0"
+  end
+end
